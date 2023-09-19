3b23c49dd6271cec9f0e89887ffb589f9b6b57a4
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-20 01:32:30 +0530

    SMS support

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from "express";
 import multer from "multer"
 import fs from "fs";
+import twilio from "twilio";
 
 const directoryPath = 'documents';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+const accountSid = 'ACcd1284ff9c91faa4f72ecb15f25de1dd';
+const authToken = 'b855e05cfe38977fcadfec2ee6565370';
+const client = twilio(accountSid, authToken);
 
 fs.mkdir(directoryPath, { recursive: true }, (err) => {
     if (err) {
@@ -11,12 +21,6 @@ fs.mkdir(directoryPath, { recursive: true }, (err) => {
         console.log('Directory created successfully.');
     }
 });
-
-
-const app = express();
-const port = 3000;
-
-app.use(express.static('public'));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -29,10 +33,24 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 app.get("/",(req,res)=>{
-    res.sendFile("public/index.html")
+    res.render("home.ejs")
 })
 
-// Handle file upload
+app.get("/claims",(req,res)=>{
+    res.render("claims.ejs");
+})
+
+app.post("/claims",(req,res)=>{
+    client.messages
+        .create({
+            body: 'Alpha2 testing enabled.',
+            from: '+12564483528',
+            to: '+918002215433',
+        })
+        .then((message) => console.log('SMS sent:', message.sid))
+        .catch((error) => console.error('Error sending SMS:', error)).then(()=>{res.render("home.ejs")});
+})
+
 app.post('/upload', upload.single('file'), (req, res) => {
     if (!req.file) {
         return res.status(400).send('No file uploaded.');
